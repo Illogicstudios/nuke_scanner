9a6c481d863f52a3c2ce8a61f1a68a3b651b1b80
mariusjenin <marius2401@laposte.net>
2023-04-28 16:36:23 +0200

    Nuke Scanner

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import importlib
 from common import utils
 
-# TODO Rename the package
-utils.unload_packages(silent=True, package="template")
-importlib.import_module("template")
-# TODO rename app
-from template.MayaTool import MayaTool
-try:
-    app.close()
-except:
-    pass
-app = MayaTool()
-app.show()
+utils.unload_packages(silent=True, package="nuke_scanner")
+importlib.import_module("nuke_scanner")
+from nuke_scanner.NukeScanner import NukeScanner
+NukeScanner().run()
